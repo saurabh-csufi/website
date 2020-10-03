saurabh-csufi/website

commit f48f56a81ab2555b47e0fdf635242f7ae9ff0624

[place] Embed chart enhancements (#472)
* move svg chart styling inline from css (so that all styling information is self-contained in the svg)
* added xml namespace attributes to svg
* include title and provenance information into a hidden svg image, which is then used to get the final svg xml for  download and embed
* svg chart is now embedded as an image so that it can be copied
* svg is now downloadable as a standalone image

static/js/chart/draw.ts
@@ -46,6 +46,12 @@ const YLABEL = {
   topMargin: 10,
   height: 15,
 };
+const SVGNS = "http://www.w3.org/2000/svg";
+const XLINKNS = "http://www.w3.org/1999/xlink";
+
+const TEXT_FONT_FAMILY = "Roboto";
+const AXIS_TEXT_FILL = "#2b2929";
+const AXIS_GRID_FILL = "#999";
 
 function appendLegendElem(
   elem: string,
@@ -150,6 +156,8 @@ function addXAxis(
     .append("g")
     .attr("class", "x axis")
     .attr("transform", `translate(0, ${chartHeight - MARGIN.bottom})`)
+    .style("stroke", AXIS_GRID_FILL)
+    .style("stroke-width", "0.5px")
     .call(d3Axis)
     .call((g) => g.select(".domain").remove());
 
@@ -162,7 +170,12 @@ function addXAxis(
       .attr("dy", ".15em")
       .attr("transform", "rotate(-35)");
   } else if (typeof xScale.bandwidth === "function") {
-    axis.selectAll(".tick text").call(wrap, xScale.bandwidth());
+    axis
+      .selectAll(".tick text")
+      .style("fill", AXIS_TEXT_FILL)
+      .style("font-family", TEXT_FONT_FAMILY)
+      .style("shape-rendering", "crispEdges")
+      .call(wrap, xScale.bandwidth());
   }
 
   let axisHeight = axis.node().getBBox().height;
@@ -213,13 +226,26 @@ function addYAxis(
     )
     .call((g) => g.select(".domain").remove())
     .call((g) =>
-      g.selectAll(".tick:not(:first-of-type) line").attr("class", "grid-line")
+      g
+        .selectAll("line")
+        .style("stroke", AXIS_GRID_FILL)
+        .style("stroke-width", "0.5")
+    )
+    .call((g) =>
+      g
+        .selectAll(".tick:not(:first-of-type) line")
+        .attr("class", "grid-line")
+        .style("stroke-opacity", "0.5")
+        .style("stroke-dasharray", "2, 2")
     )
     .call((g) =>
       g
         .selectAll(".tick text")
         .attr("x", -width + MARGIN.left + MARGIN.yAxis)
         .attr("dy", -4)
+        .style("fill", AXIS_TEXT_FILL)
+        .style("font-family", TEXT_FONT_FAMILY)
+        .style("shape-rendering", "crispEdges")
     );
 }
 
@@ -259,6 +285,8 @@ function drawHistogram(
   const svg = d3
     .select("#" + id)
     .append("svg")
+    .attr("xmlns", SVGNS)
+    .attr("xmlns:xlink", XLINKNS)
     .attr("width", width)
     .attr("height", height);
 
@@ -299,6 +327,8 @@ function drawSingleBarChart(
   const svg = d3
     .select("#" + id)
     .append("svg")
+    .attr("xmlns", SVGNS)
+    .attr("xmlns:xlink", XLINKNS)
     .attr("width", chartWidth)
     .attr("height", chartHeight);
 
@@ -363,6 +393,8 @@ function drawStackBarChart(
   const svg = d3
     .select("#" + id)
     .append("svg")
+    .attr("xmlns", SVGNS)
+    .attr("xmlns:xlink", XLINKNS)
     .attr("width", chartWidth)
     .attr("height", chartHeight);
 
@@ -440,6 +472,8 @@ function drawGroupBarChart(
   const svg = d3
     .select("#" + id)
     .append("svg")
+    .attr("xmlns", SVGNS)
+    .attr("xmlns:xlink", XLINKNS)
     .attr("width", chartWidth)
     .attr("height", chartHeight);
 
@@ -481,6 +515,8 @@ function drawGroupBarChart(
       return !!labelToLink[d3.select(this).text()];
     })
     .attr("class", "place-tick")
+    .style("cursor", "pointer")
+    .style("text-decoration", "underline")
     .on("click", function (this) {
       window.open(labelToLink[d3.select(this).text()], "_blank");
     });
@@ -512,6 +548,8 @@ function drawLineChart(
   const svg = d3
     .select("#" + id)
     .append("svg")
+    .attr("xmlns", SVGNS)
+    .attr("xmlns:xlink", XLINKNS)
     .attr("width", width)
     .attr("height", height);
 
@@ -556,8 +594,12 @@ function drawLineChart(
         .append("path")
         .datum(dataset.filter(line.defined())) // Only plot points that are defined
         .attr("class", "line fill")
+        .attr("d", line)
+        .style("fill", "none")
+        .style("stroke-width", "2.5px")
         .style("stroke", colorFn(dataGroup.label))
-        .attr("d", line);
+        .style("opacity", 0.4)
+        .style("stroke-dasharray", 2);
     }
 
     svg
@@ -565,7 +607,10 @@ function drawLineChart(
       .datum(dataset)
       .attr("class", "line")
       .style("stroke", colorFn(dataGroup.label))
-      .attr("d", line);
+      .attr("d", line)
+      .style("fill", "none")
+      .style("stroke-width", "2.5px")
+      .style("stroke", colorFn(dataGroup.label));
 
     if (shouldAddDots) {
       svg
@@ -577,8 +622,9 @@ function drawLineChart(
         .attr("class", "dot")
         .attr("cx", (d) => xScale(d[0]))
         .attr("cy", (d) => yScale(d[1]))
-        .attr("fill", colorFn(dataGroup.label))
-        .attr("r", (d) => (d[1] === null ? 0 : 3));
+        .attr("r", (d) => (d[1] === null ? 0 : 3))
+        .style("fill", colorFn(dataGroup.label))
+        .style("stroke", "#fff");
     }
   }
 
@@ -672,6 +718,8 @@ function drawGroupLineChart(
 
   const svg = container
     .append("svg")
+    .attr("xmlns", SVGNS)
+    .attr("xmlns:xlink", XLINKNS)
     .attr("width", width)
     .attr("height", height + SOURCE.height);
 
@@ -695,6 +743,7 @@ function drawGroupLineChart(
     .attr("class", "label")
     .attr("text-anchor", "start")
     .attr("transform", `translate(${MARGIN.grid}, ${YLABEL.topMargin})`)
+    .style("font-size", "12px")
     .text(ylabel);
 
   for (const place in dataGroupsDict) {

static/js/place/chart.tsx
@@ -148,7 +148,7 @@ class Chart extends React.Component<ChartPropType, ChartStateType> {
             </div>
             <div className="outlinks">
               <a href="#" onClick={this._handleEmbed}>
-                Embed
+                Export
               </a>
               <a className="explore-more" href={exploreUrl}>
                 Explore More ›
@@ -214,16 +214,22 @@ class Chart extends React.Component<ChartPropType, ChartStateType> {
     e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
   ): void {
     e.preventDefault();
-    // Node does not have innerHTML property so we need to pass both in.
     const svgElems = this.svgContainerElement.current.getElementsByTagName(
       "svg"
     );
-    let svgHtml: string;
+    let svgXml: string;
     if (svgElems.length) {
-      svgHtml = svgElems.item(0).innerHTML;
+      svgXml = svgElems.item(0).outerHTML;
     }
-    const svgDom = this.chartElement.current.cloneNode(true);
-    this.embedModalElement.current.show(svgHtml, svgDom, this.dataCsv());
+    this.embedModalElement.current.show(
+      svgXml,
+      this.dataCsv(),
+      this.svgContainerElement.current.offsetWidth,
+      CHART_HEIGHT,
+      this.props.title,
+      this.props.snapshot ? this.props.snapshot.date : "",
+      this.props.snapshot ? this.props.snapshot.sources : []
+    );
   }
 
   drawChart(): void {

static/css/draw.scss
@@ -14,42 +14,6 @@
  * limitations under the License.
  */
 
-path.line {
-  fill: none;
-  stroke-width: 2.5;
-}
-
-path.line.fill {
-  opacity: .4;
-  stroke-dasharray: 2;
-}
-
-.axis line {
-  stroke: #999;
-  stroke-width: 0.5;
-}
-
-.axis path {
-  stroke: #999;
-  stroke-width: 0.5;
-  shape-rendering: crispEdges;
-}
-
-.axis text {
-  fill: #2b2929;
-  font-family: Roboto;
-  shape-rendering: crispEdges;
-}
-
-.y.axis line.grid-line {
-  stroke-opacity: 0.5;
-  stroke-dasharray: 2, 2;
-}
-
-.dot {
-  stroke: #fff;
-}
-
 .legend {
   margin-left: 35px;
   text-align: center;
@@ -72,13 +36,4 @@ path.line.fill {
 
 .legend-text {
   font-size: 14px;
-}
-
-.label {
-  font-size: 12px;
-}
-
-.place-tick {
-  cursor: pointer;
-  text-decoration: underline;
 }

static/css/place/place.scss
@@ -196,7 +196,7 @@ $chart-border: 0.5px solid #dee2e6;
 
 .factoid footer,
 .chart-container footer {
-  font-size: 0.5rem;
+  font-size: 0.7rem;
   color: var(--dc-gray);
   margin-top: 0.5rem;
 }
@@ -278,7 +278,7 @@ $chart-border: 0.5px solid #dee2e6;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 0 4px;
+  padding: 0 10px;
 }
 
 .outlinks a {
@@ -315,13 +315,22 @@ $chart-border: 0.5px solid #dee2e6;
   display: none;
 }
 
-.modal .copy-svg {
+.modal .modal-chart-container > svg {
+  visibility: hidden;
+}
+
+.modal .modal-chart-container img {
+  border-radius: 3px;
+  border: $chart-border;
+}
+
+.modal textarea {
   background: #efefef;
   border-radius: 3px;
   border: $chart-border;
   display: flex;
   font-family: monospace;
-  height: 6rem;
+  height: 8rem;
   margin: auto;
   padding: .5rem;
 }

static/js/place/chart_embed.tsx
@@ -17,12 +17,25 @@
 import React from "react";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import { randDomId, saveToFile } from "../shared/util";
+import * as d3 from "d3";
+
+// SVG adjustment related constants
+const TITLE_HEIGHT = 20;
+const TITLE_MARGIN = 10;
+const SOURCES_HEIGHT = 10;
+const SOURCES_MARGIN = 30;
+const SVGNS = "http://www.w3.org/2000/svg";
+const XLINKNS = "http://www.w3.org/1999/xlink";
 
 interface ChartEmbedStateType {
   modal: boolean;
-  chartDom: Node;
-  svgHtml: string;
+  svgXml: string;
   dataCsv: string;
+  chartWidth: number;
+  chartHeight: number;
+  chartTitle: string;
+  chartDate: string;
+  sources: string[];
 }
 
 /**
@@ -30,23 +43,32 @@ interface ChartEmbedStateType {
  * in a Modal
  */
 class ChartEmbed extends React.Component<unknown, ChartEmbedStateType> {
-  private textareaElement: React.RefObject<HTMLTextAreaElement>;
+  private chartDownloadXml: string;
   private modalId: string;
+  private svgContainerElement: React.RefObject<HTMLDivElement>;
+  private textareaElement: React.RefObject<HTMLTextAreaElement>;
 
   constructor(props: unknown) {
     super(props);
     this.state = {
       modal: false,
-      svgHtml: "",
-      chartDom: null,
+      svgXml: "",
       dataCsv: "",
+      chartWidth: 0,
+      chartHeight: 0,
+      chartTitle: "",
+      chartDate: "",
+      sources: [],
     };
+    this.chartDownloadXml = "";
     this.modalId = randDomId();
+    this.svgContainerElement = React.createRef();
     this.textareaElement = React.createRef();
+
     this.toggle = this.toggle.bind(this);
     this.onOpened = this.onOpened.bind(this);
-    this.copySvgToClipboard = this.copySvgToClipboard.bind(this);
-    this.downloadData = this.downloadData.bind(this);
+    this.onDownloadSvg = this.onDownloadSvg.bind(this);
+    this.onDownloadData = this.onDownloadData.bind(this);
     this.onClickTextarea = this.onClickTextarea.bind(this);
   }
 
@@ -62,36 +84,109 @@ class ChartEmbed extends React.Component<unknown, ChartEmbedStateType> {
   /**
    * Updates the view state of the modal to true, and includes the data necessary for displaying the modal.
    */
-  public show(svgHTML: string, chartDOM: Node, dataCsv: string): void {
+  public show(
+    svgXml: string,
+    dataCsv: string,
+    chartWidth: number,
+    chartHeight: number,
+    chartTitle: string,
+    chartDate: string,
+    sources: string[]
+  ): void {
     this.setState({
       modal: true,
-      svgHtml: svgHTML,
-      chartDom: chartDOM,
+      svgXml: svgXml,
       dataCsv: dataCsv,
+      chartWidth: chartWidth,
+      chartHeight: chartHeight,
+      chartTitle: chartTitle,
+      chartDate: chartDate ? "(" + chartDate + ")" : "",
+      sources: sources,
     });
+  }
+
+  /**
+   * Decorates svgXml with title and provenance information embeded in an enclosing
+   * SVG node. Returns the SVG contents as a string.
+   */
+  private decorateSvgChart(): string {
+    const container = this.svgContainerElement.current;
+    container.innerHTML = "";
+
+    // Decorate a hidden chart svg with title and provenance
+    const svg = d3
+      .select(container)
+      .append("svg")
+      .attr("xmlns", SVGNS)
+      .attr("xmlns:xlink", XLINKNS)
+      .attr("width", this.state.chartWidth)
+      .attr(
+        "height",
+        this.state.chartHeight +
+          TITLE_HEIGHT +
+          TITLE_MARGIN +
+          SOURCES_HEIGHT +
+          SOURCES_MARGIN
+      );
+
+    svg
+      .append("g")
+      .attr(
+        "transform",
+        `translate(${this.state.chartWidth / 2}, ${TITLE_HEIGHT})`
+      )
+      .append("text")
+      .style("font-family", "sans-serif")
+      .style("fill", "#3b3b3b")
+      .style("font-size", ".85rem")
+      .style("font-weight", "bold")
+      .style("text-anchor", "middle")
+      .text(`${this.state.chartTitle} ${this.state.chartDate}`);
+
+    svg
+      .append("g")
+      .attr("transform", `translate(0, ${TITLE_HEIGHT + TITLE_MARGIN})`)
+      .append("svg")
+      .html(this.state.svgXml);
+
+    svg
+      .append("g")
+      .attr(
+        "transform",
+        `translate(5, ${
+          TITLE_HEIGHT + TITLE_MARGIN + this.state.chartHeight + SOURCES_MARGIN
+        })`
+      )
+      .append("text")
+      .style("fill", "#3b3b3b")
+      .style("font-family", "sans-serif")
+      .style("font-size", ".7rem")
+      .text(`Data from ${this.state.sources.join(",")} via Data Commons`);
+
+    const svgXml = svg.node().outerHTML;
+    container.innerHTML = "";
+
+    return svgXml;
   }
 
   /**
    * Callback for after the modal has been rendered and added to the DOM.
    */
   public onOpened(): void {
-    const modalElement = document.getElementById(this.modalId);
-    if (!modalElement || !this.state.chartDom) {
+    if (!this.svgContainerElement.current) {
       return;
     }
-    // Append cloned chart DOM to the modal.
-    const containerElem = modalElement.querySelector(".modal-chart-container");
-    if (containerElem) {
-      containerElem.appendChild(this.state.chartDom);
-      const chartElem = containerElem.querySelector(".chart-container");
-      if (chartElem) {
-        // Update width of textarea to match the width of the chart.
-        const textarea = modalElement.querySelector("textarea");
-        if (textarea) {
-          textarea.style.width = chartElem.clientWidth + "px";
-        }
-      }
+    if (this.textareaElement.current) {
+      this.textareaElement.current.style.width = this.state.chartWidth + "px";
     }
+
+    this.chartDownloadXml = this.decorateSvgChart();
+
+    const imageElement = document.createElement("img");
+    const chartBase64 =
+      "data:image/svg+xml;base64," + btoa(this.chartDownloadXml);
+    imageElement.src = chartBase64;
+    this.svgContainerElement.current.append(imageElement);
   }
 
   /**
@@ -108,25 +203,14 @@ class ChartEmbed extends React.Component<unknown, ChartEmbedStateType> {
   /**
    * On click handler for "Copy SVG to clipboard button".
    */
-  public copySvgToClipboard(): void {
-    const currentFocus = document.activeElement;
-    this.onClickTextarea();
-    document.execCommand("copy");
-
-    // restore original focus
-    if (
-      currentFocus &&
-      currentFocus instanceof HTMLElement &&
-      typeof currentFocus.focus === "function"
-    ) {
-      currentFocus.focus();
-    }
+  public onDownloadSvg(): void {
+    saveToFile("chart.svg", this.chartDownloadXml);
   }
 
   /**
    * On click handler for "Download Data" button.
    */
-  public downloadData(): void {
+  public onDownloadData(): void {
     saveToFile("export.csv", this.state.dataCsv);
   }
 
@@ -139,23 +223,26 @@ class ChartEmbed extends React.Component<unknown, ChartEmbedStateType> {
         onOpened={this.onOpened}
         id={this.modalId}
       >
-        <ModalHeader toggle={this.toggle}>Embed this chart</ModalHeader>
+        <ModalHeader toggle={this.toggle}>Export this chart</ModalHeader>
         <ModalBody>
-          <div className="modal-chart-container"></div>
+          <div
+            ref={this.svgContainerElement}
+            className="modal-chart-container"
+          ></div>
           <textarea
             className="copy-svg mt-3"
-            value={this.state.svgHtml}
+            value={this.state.dataCsv}
             readOnly
             ref={this.textareaElement}
             onClick={this.onClickTextarea}
           ></textarea>
         </ModalBody>
         <ModalFooter>
-          <Button color="primary" onClick={this.copySvgToClipboard}>
-            Copy SVG to clipboard
+          <Button color="primary" onClick={this.onDownloadSvg}>
+            Download Chart Image
           </Button>{" "}
-          <Button color="primary" onClick={this.downloadData}>
-            Download Data
+          <Button color="primary" onClick={this.onDownloadData}>
+            Download Data as CSV
           </Button>
         </ModalFooter>
       </Modal>

static/js/shared/util.js
@@ -500,14 +500,21 @@ function randDomId() {
 /**
  * Saves csv to filename.
  * @param {filename} string
- * @param {csv} string
+ * @param {contents} string
+ * @param {=header} string optional file header to prepend to the file contents
  * @return void
  */
-function saveToFile(filename, csv) {
-  if (!csv.match(/^data:text\/csv/i)) {
-    csv = "data:text/csv;charset=utf-8," + csv;
+function saveToFile(filename, contents) {
+  if (filename.match(/\.csv$/i)) {
+    if (!contents.match(/^data:text\/csv/i)) {
+      contents = "data:text/csv;charset=utf-8," + contents;
+    }
+  } else if (filename.match(/\.svg$/i)) {
+    if (!contents.match(/^data:image\/svg/i)) {
+      contents = "data:image/svg+xml;charset=utf-8," + contents;
+    }
   }
-  const data = encodeURI(csv);
+  const data = encodeURI(contents);
   const link = document.createElement("a");
   link.setAttribute("href", data);
   link.setAttribute("download", filename);

commit 04453094c594980d4e9c90b2d7311f077a4530f2

Some chart rendering fixes (#475)
* Fix overlapping stack bar charts when there is incomplete stat var data
* Highlight the SVG rect of the current place (it's quite subtle so we might want to tweak it)
* Make place labels clickable again (also applied to stack bar charts)
* Fix label rendering crispness and timelines tool (regression from #472)

server/templates/place.html
@@ -66,6 +66,20 @@
     </div>
   </div>
   <div id="modal"></div>
+  {#- Add an SVG filter to lighten bars for comparison places -#}
+  <style>
+    g rect.g-bar[data-dcid="{{ place_dcid }}"] {
+      filter: url(#lighten);
+    }
+  </style>
+  <svg width=0 height=0>
+    <filter id="lighten">
+      <feColorMatrix type="matrix" values="1.25  0     0    0  0
+                                           0     1.25  0    0  0
+                                           0     0     1.25 0  0
+                                           0     0     0    1  0" />
+    </filter>
+  </svg>
 {% endblock %}
 
 {% block footer %}

static/js/chart/base.ts
@@ -21,9 +21,12 @@ const DEFAULT_COLOR = "#000";
 class DataPoint {
   value: number;
   label: string;
-  constructor(label: string, value: number) {
+  // Optional DCID to add to a chart as a data atttribute
+  dcid?: string;
+  constructor(label: string, value: number, dcid?: string) {
     this.value = value;
     this.label = label;
+    this.dcid = dcid;
   }
 }
 

static/js/chart/draw.ts
@@ -106,6 +106,7 @@ function wrap(
     let word: string;
     word = words.pop();
     while (word) {
+      word = word.trim();
       let separator = getWrapLineSeparator(line);
       line.push(word);
       tspan.text(line.join(separator));
@@ -156,16 +157,21 @@ function addXAxis(
     .append("g")
     .attr("class", "x axis")
     .attr("transform", `translate(0, ${chartHeight - MARGIN.bottom})`)
-    .style("stroke", AXIS_GRID_FILL)
-    .style("stroke-width", "0.5px")
     .call(d3Axis)
-    .call((g) => g.select(".domain").remove());
+    .call((g) => g.select(".domain").remove())
+    .call((g) =>
+      g
+        .selectAll("line")
+        .attr("stroke", AXIS_GRID_FILL)
+        .attr("stroke-width", "0.5")
+    );
 
   if (shouldRotate) {
     axis
       .attr("transform", `translate(0, ${chartHeight - ROTATE_MARGIN_BOTTOM})`)
       .selectAll("text")
       .style("text-anchor", "end")
+      .style("text-rendering", "optimizedLegibility")
       .attr("dx", "-.8em")
       .attr("dy", ".15em")
       .attr("transform", "rotate(-35)");
@@ -174,7 +180,7 @@ function addXAxis(
       .selectAll(".tick text")
       .style("fill", AXIS_TEXT_FILL)
       .style("font-family", TEXT_FONT_FAMILY)
-      .style("shape-rendering", "crispEdges")
+      .style("text-rendering", "optimizedLegibility")
       .call(wrap, xScale.bandwidth());
   }
 
@@ -355,6 +361,8 @@ function drawSingleBarChart(
     .selectAll("rect")
     .data(dataPoints)
     .join("rect")
+    .classed("g-bar", true)
+    .attr("data-dcid", (d) => d.dcid)
     .attr("x", (d) => x(d.label))
     .attr("y", (d) => y(d.value))
     .attr("width", x.bandwidth())
@@ -378,6 +386,11 @@ function drawStackBarChart(
   dataGroups: DataGroup[],
   unit?: string
 ): void {
+  const labelToLink = {};
+  for (const dataGroup of dataGroups) {
+    labelToLink[dataGroup.label] = dataGroup.link;
+  }
+
   const keys = dataGroups[0].value.map((dp) => dp.label);
 
   const data = [];
@@ -385,6 +398,7 @@ function drawStackBarChart(
     const curr: { [property: string]: any } = { label: dataGroup.label };
     for (const dataPoint of dataGroup.value) {
       curr[dataPoint.label] = dataPoint.value;
+      curr.dcid = dataPoint.dcid;
     }
     data.push(curr);
   }
@@ -428,12 +442,28 @@ function drawStackBarChart(
     .selectAll("rect")
     .data((d) => d)
     .join("rect")
+    .classed("g-bar", true)
+    .attr("data-dcid", (d) => d.data.dcid)
     .attr("x", (d) => x(String(d.data.label)))
     .attr("y", (d) => (Number.isNaN(d[1]) ? y(d[0]) : y(d[1])))
     .attr("width", x.bandwidth())
     .attr("height", (d) => (Number.isNaN(d[1]) ? 0 : y(d[0]) - y(d[1])));
 
   appendLegendElem(id, color, keys);
+
+  // Add link to place name labels.
+  svg
+    .select(".x.axis")
+    .selectAll(".tick text")
+    .filter(function (this) {
+      return !!labelToLink[d3.select(this).text()];
+    })
+    .attr("class", "place-tick")
+    .style("cursor", "pointer")
+    .style("text-decoration", "underline")
+    .on("click", function (this) {
+      window.open(labelToLink[d3.select(this).text()], "_blank");
+    });
 }
 
 /**
@@ -498,8 +528,12 @@ function drawGroupBarChart(
     .join("g")
     .attr("transform", (dg) => `translate(${x0(dg.label)},0)`)
     .selectAll("rect")
-    .data((dg) => dg.value.map((dp) => ({ key: dp.label, value: dp.value })))
+    .data((dg) =>
+      dg.value.map((dp) => ({ key: dp.label, value: dp.value, dcid: dp.dcid }))
+    )
     .join("rect")
+    .classed("g-bar", true)
+    .attr("data-dcid", (d) => d.dcid)
     .attr("x", (d) => x1(d.key))
     .attr("y", (d) => y(d.value))
     .attr("width", x1.bandwidth())
@@ -745,6 +779,7 @@ function drawGroupLineChart(
     .attr("text-anchor", "start")
     .attr("transform", `translate(${MARGIN.grid}, ${YLABEL.topMargin})`)
     .style("font-size", "12px")
+    .style("text-rendering", "optimizedLegibility")
     .text(ylabel);
 
   for (const place in dataGroupsDict) {
@@ -765,10 +800,11 @@ function drawGroupLineChart(
         .append("path")
         .datum(dataset)
         .attr("class", "line")
-        .style("stroke", lineStyle.color)
         .attr("d", line)
-        .attr("stroke-width", "1")
-        .attr("stroke-dasharray", lineStyle.dash);
+        .style("fill", "none")
+        .style("stroke", lineStyle.color)
+        .style("stroke-width", "2px")
+        .style("stroke-dasharray", lineStyle.dash);
     }
   }
   // add source info to the chart
@@ -777,12 +813,14 @@ function drawGroupLineChart(
     svg
       .append("text")
       .attr("class", "label")
-      .attr("text-anchor", "start")
       .attr(
         "transform",
         `translate(${MARGIN.grid}, ${height + SOURCE.topMargin})`
       )
-      .attr("fill", "#808080")
+      .style("fill", "#808080")
+      .style("font-size", "12px")
+      .style("text-anchor", "start")
+      .style("text-rendering", "optimizedLegibility")
       .text(sourceText);
   }
 
@@ -824,8 +862,8 @@ function buildInChartLegend(
         .attr("class", "legend-line")
         .attr("x1", "0")
         .attr("x2", `${LEGEND.dashWidth - 3}`)
-        .attr("stroke", LEGEND.defaultColor)
-        .attr("stroke-dasharray", `${legendStyle.dash}`);
+        .style("stroke", LEGEND.defaultColor)
+        .style("stroke-dasharray", `${legendStyle.dash}`);
       dashWidth = LEGEND.dashWidth;
     }
     // Draw the text.
@@ -836,7 +874,8 @@ function buildInChartLegend(
       .attr("y", "0.3em")
       .attr("dy", "0")
       .text(label)
-      .attr("fill", `${legendStyle.color}`)
+      .style("text-rendering", "optimizedLegibility")
+      .style("fill", `${legendStyle.color}`)
       .call(wrap, legendTextdWidth);
     yOffset += lgGroup.node().getBBox().height + LEGEND.lineMargin;
   }

static/js/dev.ts
@@ -72,24 +72,24 @@ window.onload = () => {
       new DataPoint("2013", 24000),
     ]),
     new DataGroup("San Francisco", [
-      new DataPoint("2011", 21000),
+      new DataPoint("2011", null),
       new DataPoint("2012", 25000),
       new DataPoint("2013", 22000),
     ]),
     new DataGroup("Mountain View", [
-      new DataPoint("2011", 1000),
+      new DataPoint("2011", null),
       new DataPoint("2012", 5000),
       new DataPoint("2013", 2000),
     ]),
     new DataGroup("Very-Long-City-Name", [
       new DataPoint("2011", 1000),
       new DataPoint("2012", 5000),
-      new DataPoint("2013", 2000),
+      new DataPoint("2013", null),
     ]),
     new DataGroup("Multi several very long city name long", [
       new DataPoint("2011", 1000),
       new DataPoint("2012", 5000),
-      new DataPoint("2013", 2000),
+      new DataPoint("2013", null),
     ]),
   ];
 

static/js/place/chart.tsx
@@ -70,6 +70,10 @@ interface ChartPropType {
    * Scale number
    */
   scaling?: number;
+  /**
+   * All stats vars for this chart
+   */
+  statsVars: string[];
 }
 
 interface ChartStateType {
@@ -325,11 +329,15 @@ class Chart extends React.Component<ChartPropType, ChartStateType> {
         });
         break;
       case chartTypeEnum.SINGLE_BAR:
-        for (const statVar in this.props.snapshot.data[0].data) {
-          dataPoints.push({
-            label: STATS_VAR_LABEL[statVar],
-            value: this.props.snapshot.data[0].data[statVar] * scaling,
-          });
+        {
+          const snapshotData = this.props.snapshot.data[0];
+          for (const statVar in snapshotData.data) {
+            dataPoints.push({
+              label: STATS_VAR_LABEL[statVar],
+              value: snapshotData.data[statVar] * scaling,
+              dcid: snapshotData.dcid,
+            });
+          }
         }
         this.setState({
           dataPoints,
@@ -340,14 +348,20 @@ class Chart extends React.Component<ChartPropType, ChartStateType> {
       case chartTypeEnum.STACK_BAR:
         for (const placeData of this.props.snapshot.data) {
           const dataPoints: DataPoint[] = [];
-          for (const statVar in placeData.data) {
+          for (const statVar of this.props.statsVars) {
+            const val = placeData.data[statVar];
             dataPoints.push({
               label: STATS_VAR_LABEL[statVar],
-              value: placeData.data[statVar] * scaling,
+              value: val ? val * scaling : null,
+              dcid: placeData.dcid,
             });
           }
           dataGroups.push(
-            new DataGroup(this.props.names[placeData.dcid], dataPoints)
+            new DataGroup(
+              this.props.names[placeData.dcid],
+              dataPoints,
+              `/place?dcid=${placeData.dcid}`
+            )
           );
         }
         this.setState({

static/js/place/chart_block.tsx
@@ -74,6 +74,7 @@ class ChartBlock extends React.Component<ChartBlockPropType, unknown> {
           unit={this.props.data.unit}
           names={this.props.names}
           scaling={this.props.data.scaling}
+          statsVars={this.props.data.statsVars}
         ></Chart>
       );
     }
@@ -109,6 +110,7 @@ class ChartBlock extends React.Component<ChartBlockPropType, unknown> {
         unit: unit,
         names: this.props.names,
         scaling: scaling,
+        statsVars: this.props.data.statsVars,
       };
       if (this.props.isOverview) {
         // Show child place(state) chart for USA page, otherwise show nearby